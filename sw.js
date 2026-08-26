75f9fbca2a4be59e586baef5cae4c74a88405d17
Alvaro <127940641+AlvaCham@users.noreply.github.com>
2026-08-26 23:52:21 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'poolpi-shell-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './img/Logo.png',
+  './img/icon-192.png',
+  './img/icon-512.png',
+  './img/piscina1.png',
+  './img/panelsolar3.png',
+  './img/wifi.svg',
+  './img/Diana99.svg',
+  './img/matraz3.svg',
+  './img/Conductividad.svg',
+  './img/QRP.svg',
+  './img/clorolibre.svg',
+  './img/solar_power.svg',
+  './img/valvula.svg',
+  './img/Depuradora.svg',
+  './img/termometro.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+
+  if (url.pathname.includes('/api/')) {
+    e.respondWith(
+      fetch(e.request).catch(() => {
+        return new Response(JSON.stringify({ disponible: false, error: 'offline' }), {
+          headers: { 'Content-Type': 'application/json' }
+        });
+      })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+  }
+});
